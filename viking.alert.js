ffb8e845d0a20acda6d1d10acb8d23440a961481
Jono O'Shea <jono.oshea@gmail.com>
2014-10-30 10:20:27 -0700

    Added basic styles and subtext to alerts

--- viking.alert.js
+++ viking.alert.js
@@ -13,13 +13,15 @@ Viking.Alert = Viking.Dialog.extend({
 
         model = {};
         model.title = options.title;
+        model.text = options.hasOwnProperty('text') ? options.text : false;
         model.posText = options.hasOwnProperty('positiveButtonText') ? options.positiveButtonText : 'Okay';
         model.negText = options.hasOwnProperty('negativeButtonText') ? options.negativeButtonText : 'Cancel';
 
         var AlertView = Backbone.View.extend({
             template : _.template(
                 '<h1><%= title %></h1>' +
-                '<button class="viking-alert-btn-positive"><%= posText %></button>'
+                '<% if (text) { %><p><%= text %></p><% } %>' +
+                '<button class="viking-alert-btn-positive primary"><%= posText %></button>'
             ),
             render : function() {
                 this.$el.html( this.template(this.model) );
@@ -38,7 +40,8 @@ Viking.Alert = Viking.Dialog.extend({
         var ConfirmView = AlertView.extend({
             template : _.template(
                 '<h1><%= title %></h1>' +
-                '<button class="viking-alert-btn-positive"><%= posText %></button>' +
+                '<% if (text) { %><p><%= text %></p><% } %>' +
+                '<button class="viking-alert-btn-positive primary"><%= posText %></button>' +
                 '<button class="viking-alert-btn-negative"><%= negText %></button>'
             ),
             render : function() {
@@ -66,6 +69,8 @@ Viking.Alert = Viking.Dialog.extend({
             view : new viewClass({ model : model }),
             model : model
         });
+
+        this.$el.addClass('viking-alert');
 
         this.constructor.__super__.initialize.apply(this, [options]);
     },
